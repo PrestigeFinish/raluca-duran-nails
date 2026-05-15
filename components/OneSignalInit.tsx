7be5f51e3043bbd13fbc25c5a3c624b46e0ce6b4
PrestigeFinish <sebiseb2438@yahoo.com>
2026-05-15 15:29:22 +0300

    Create OneSignalInit.tsx

---
+++ components/OneSignalInit.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import OneSignal from "react-onesignal";
+
+export default function OneSignalInit() {
+  useEffect(() => {
+    OneSignal.init({
+      appId: "82336a62-54d3-4c3b-951f-f619653fbf94",
+      notifyButton: {
+        enable: true,
+      },
+    });
+  }, []);
+
+  return null;
+}
