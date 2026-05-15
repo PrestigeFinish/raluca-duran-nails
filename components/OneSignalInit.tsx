427182c2eb7d892a965a9e2da3cccf5b4ebacaf1
PrestigeFinish <sebiseb2438@yahoo.com>
2026-05-15 14:45:45 +0300

    Update OneSignalInit.tsx

--- components/OneSignalInit.tsx
+++ components/OneSignalInit.tsx
@@ -1,24 +1,14 @@
 "use client";
 
 import { useEffect } from "react";
+import OneSignal from "react-onesignal";
 
 export default function OneSignalInit() {
   useEffect(() => {
-    async function initOneSignal() {
-      if (typeof window === "undefined") return;
-
-      const OneSignal = (await import("react-onesignal")).default;
-
-      await OneSignal.init({
-        appId: "82336a62-54d3-4c3b-951f-f619653fbf94",
-        allowLocalhostAsSecureOrigin: true,
-        notifyButton: {
-          enable: true,
-        },
-      });
-    }
-
-    initOneSignal();
+    OneSignal.init({
+      appId: "82336a62-54d3-4c3b-951f-f619653fbf94",
+      allowLocalhostAsSecureOrigin: true,
+    });
   }, []);
 
   return null;
