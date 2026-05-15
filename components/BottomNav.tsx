def9d03b51c90190b49608156f8b9a36131c2392
PrestigeFinish <sebiseb2438@yahoo.com>
2026-05-15 16:50:36 +0300

    .

---
+++ components/BottomNav.tsx
@@ -0,0 +1,9 @@
+export default function BottomNav() {
+  return (
+    <nav className="bottom-nav">
+      <a href="/">Nails</a>
+      <a href="/makeup">Make-up</a>
+      <a href="/programare">Programare</a>
+    </nav>
+  );
+}
