9c4ab2b2fe671c5122c51990f3cf3bfc3856d592
PrestigeFinish <sebiseb2438@yahoo.com>
2026-05-15 17:23:59 +0300

    Update BottomNav.tsx

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -1,7 +1,7 @@
 export default function BottomNav() {
   return (
     <nav className="bottom-nav">
-      <a href="/">Nails</a>
+      <a href="/">Acasă</a>
       <a href="/makeup">Make-up</a>
       <a href="/programare">Programare</a>
     </nav>
