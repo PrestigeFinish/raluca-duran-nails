268f81dffc88901181b809aa8d372f0bdcb0333e
PrestigeFinish <sebiseb2438@yahoo.com>
2026-05-23 12:31:19 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import GallerySection from "../components/GallerySection";
 import { createClient } from "@supabase/supabase-js";
+export const dynamic = "force-dynamic";
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
